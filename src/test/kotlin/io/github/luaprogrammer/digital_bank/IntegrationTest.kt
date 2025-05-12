9e22d8affb192982e686b428558ff7628f4b90e0
Luana Melissa <luanamelissaprogrammer@gmail.com>
2025-05-11 23:25:47 -0300

    Trabalhando com o DirtiesContext, mas será retirado depois.

--- src/test/kotlin/io/github/luaprogrammer/digital_bank/IntegrationTest.kt
+++ src/test/kotlin/io/github/luaprogrammer/digital_bank/IntegrationTest.kt
@@ -4,6 +4,7 @@ import org.junit.jupiter.api.TestInstance
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.test.annotation.DirtiesContext
 import org.springframework.test.context.DynamicPropertyRegistry
 import org.springframework.test.context.DynamicPropertySource
 import org.springframework.test.web.servlet.MockMvc
@@ -16,7 +17,7 @@ import java.time.Duration
 @SpringBootTest
 @AutoConfigureMockMvc
 @Testcontainers
-@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@DirtiesContext
 abstract class IntegrationTest {
 
     @Autowired
